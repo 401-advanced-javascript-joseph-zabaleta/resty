d43f49ed8c4c909473b6531136ff3d5c8dab5a81
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-09-14 10:58:24 -0700

    Moved React Starter files to reference folder, Added App and index files

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class App extends React.Component {
+
+    render() {
+
+        return (
+
+            <div>
+                Testing
+            </div>
+
+        );
+
+    };
+
+};
+
+export default App;
