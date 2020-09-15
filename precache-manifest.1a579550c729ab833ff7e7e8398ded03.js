adf228d1101889717fab8bd3c7084473108446e3
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-09-15 11:44:06 -0700

    Updates

---
+++ precache-manifest.1a579550c729ab833ff7e7e8398ded03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83577cccb38d78b99e26ad4704fa4058",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42998e01deaaf3b22b69",
+    "url": "/static/css/main.087fe778.chunk.css"
+  },
+  {
+    "revision": "371d344da5c1ab8d861b",
+    "url": "/static/js/2.9590602b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9590602b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42998e01deaaf3b22b69",
+    "url": "/static/js/main.719d145a.chunk.js"
+  },
+  {
+    "revision": "e7b4ea4f970612a52c32",
+    "url": "/static/js/runtime-main.3acf32b8.js"
+  }
+]);
