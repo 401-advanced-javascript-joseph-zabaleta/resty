8f15634f3988ab4364ec5489db087cf84566f772
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-09-14 19:54:33 -0700

    Updated imports and add axios dependency

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './app.scss';
 
-import Header from './components/header/header.js';
-import Form from './components/form/form.js';
-import Footer from './components/footer/footer.js';
+import Header from './components/header/header.jsx';
+import Form from './components/form/form.jsx';
+import Footer from './components/footer/footer.jsx';
 
 export default class App extends React.Component {
 
